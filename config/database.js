638ad411ce466e06a08f974c09e8f351a96cef1e
Tim Berman <berman.tim@gmail.com>
2016-09-20 00:07:35 +1000

    use heroku db

--- config/database.js
+++ config/database.js
@@ -7,6 +7,6 @@ var username = "heroku_bhkj4ckg",
 
 module.exports = {
     // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    'url': 'mongodb://' + username + ':' + password + "@" + host + ":" + port + "/" + dbname
-    //'url' : 'mongodb://heroku_bhkj4ckg:na0ausnqjenr60fe883r954l6l@ds017896.mlab.com:17896/heroku_bhkj4ckg'
+    //'url': 'mongodb://' + username + ':' + password + "@" + host + ":" + port + "/" + dbname
+    'url' : 'mongodb://heroku_bhkj4ckg:na0ausnqjenr60fe883r954l6l@ds017896.mlab.com:17896/heroku_bhkj4ckg'
 };
